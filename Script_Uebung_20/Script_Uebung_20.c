e95c31e71b314d90baf163c7bda1f51a1226a788
Ragnarogk <mherzog.berlin@googlemail.com>
2016-11-26 11:02:43 +0100

    Aufgabe 20 aus dem Script

---
+++ Script_Uebung_20/Script_Uebung_20.c
@@ -0,0 +1,36 @@
+// Script_Uebung_20.c : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	unsigned char byte1, byte2, byte3;
+	int bitNr;
+
+	byte1 = 0x80;
+	byte2 = 0x7F;
+	byte3 = 0x55;
+
+	// bit setzen
+	printf("Welches Bit im Byte %Xh soll gesetzt werden? Bit Nr.: ", byte1);
+	scanf("%i%*c", &bitNr);
+	printf("Das Byte nach dem Setzten von Bit Nr. %i: %Xh\n", bitNr, byte1 | (1 << bitNr) );
+	printf("\n");
+
+	// bit rücksetzen
+	printf("Welches Bit im Byte %Xh soll rueckgesetzt werden? Bit Nr.: ", byte2);
+	scanf("%i%*c", &bitNr);
+	printf("Das Byte nach dem Ruecksetzten von Bit Nr. %i: %Xh\n", bitNr, byte2 & (~(1 << bitNr)) );
+	printf("\n");
+
+	// bit invertieren
+	printf("Welches Bit im Byte %Xh soll gesetzt werden? Bit Nr.: ", byte3);
+	scanf("%i%*c", &bitNr);
+	printf("Das Byte nach dem Setzten von Bit Nr. %i: %Xh\n", bitNr, byte3 ^ (1 << bitNr));
+	printf("\n");
+
+    return 0;
+}
+
